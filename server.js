f8aa81427a9280f7b1a262ffe7a92d013c053fd9
Ian Morton <ian.morton@sky.uk>
2016-09-23 11:27:45 +0100

    Strt up

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// web port
+var PORT = process.env.PORT || 8080; 
+//
+//=======================
+// Initialize the app.
+//
+    var server = app.listen(PORT, function () {
+        var port = server.address().port;
+        console.log("App now running on port", port);
+    });
